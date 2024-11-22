7b7162fc3a7c8d406deaae8a94e53944933f0857
Brian Larson <brian@brianlarson.com>
2024-11-22 09:41:05 -0600

    POST route finally working!?

--- server/routes/items.router.jsx
+++ server/routes/items.router.jsx
@@ -8,12 +8,13 @@ router.get("/", (req, res) => {
 
   // Set SELECT SQL statement
   const statement = `
-    SELECT * FROM "items";
+    SELECT * FROM "items" ORDER BY "name" ASC;
   `;
 
   // Send statement to db and send back items in db
   pool.query(statement)
     .then((response) => {
+      console.log('response.rows:', response.rows);
       res.send(response.rows);
   })
     .catch((error) => {
@@ -26,17 +27,21 @@ router.get("/", (req, res) => {
 // POST router to add new items to db
 router.post("/", (req, res) => {
 
-  console.log('req.body is:', req.body);
+  const item = req.body;
+  console.log('req.body as `item` is:', item);
 
   // Set INSERT SQL statement
   const statement = `
     INSERT INTO "items" ("name", "quantity", "unit")
-    VALUES ('Peaches', 2, 'Bags');
+    VALUES ('${item.name}', ${item.quantity}, '${item.unit}');
   `;
+  
+  console.log('SQL statement for POST request:', statement);
 
   // Send statement to db to add item and send back updated items
   pool.query(statement)
     .then((response) => {
+      console.log('response.data in POST route is:', response.data);
       res.sendStatus(201);
   })
     .catch((error) => {
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
- // ? ASK ABOUT ODD NEED FOR .jsx in file name
+// ? ASK ABOUT ODD NEED FOR .jsx in file name
 import Header from "../Header/Header.jsx";
 import "./App.css";
 
@@ -10,6 +10,9 @@ function App() {
 
   // Set state vars
   const [items, setItems] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newQuantity, setNewQty] = useState("");
+  const [newUnit, setNewUnit] = useState("");
 
   // Fetch items on load
   useEffect(() => {
@@ -23,8 +26,9 @@ function App() {
       url: "/api/items"
     })
       .then((response) => {
-        console.log('/api/items GET request sent successfully…');
+        console.log('/api/items GET request sent successfully…', response.data);
         setItems(response.data);
+        console.log('AFTER setItems(), items is:', items);
     })
       .catch((error) => {
         console.log("Error with GET /api/items…", error);
@@ -32,84 +36,102 @@ function App() {
   }
 
   // Add a new item to the db and show on DOM
-  const addItem = () => {
+  const addItem = (event) => {
+    event.preventDefault();
+
     axios({
       method: "POST",
       url: "/api/items",
-      data: { name: "Peaches", quantity: '3', units: 'Bags' }
+      data: { name: newName, quantity: newQuantity, unit: newUnit }
     })
       .then((response) => {
-        console.log('/api/items POST request sent successfully…');
-        setItems(response.data);
+        console.log('/api/items POST request sent successfully…', response.data);
+        
+        // Update state of items arr and re-render to DOM with fetch func
+        fetchItems();
+        console.log('AFTER addItem(), items is:', items);
+
+        // Clear inputs
+        setNewName("");
+        setNewQty("");
+        setNewUnit("");
     })
       .catch((error) => {
         console.log("Error with POST /api/items…", error);
     });
+    
   }
 
+  console.log('items is now before returning...', items);
+
   return (
-    <div className="App">
+    <div className="App notice">
+      
       <Header />
-      <main className="notice">
+
+      <main>
+      
+        {/* Form component */}
         <section>
           <h2>Add an Item</h2>
           <form>
-
-            <label htmlFor="item">
-              Item:
-            </label>
-            <input id="item" type="text" />
+            <label htmlFor="item">Item:</label>
+            <input 
+              onChange={(event) => setNewName(event.target.value)} 
+              value={newName}
+              id="item" 
+              type="text"
+            />
             
-            <label htmlFor="quantity">
-              Quantity:
-            </label>
-            <input id="quantity" type="text" />
+            <label htmlFor="quantity">Quantity:</label>
+            <input 
+              onChange={(event) => setNewQty(event.target.value)} 
+              value={newQuantity}
+              id="quantity" 
+              type="text"
+            />
 
-            <label htmlFor="item">
-              Unit:
-            </label>
-            <input id="unit" type="text" />
+            <label htmlFor="item">Unit:</label>
+            <input 
+              onChange={(event) => setNewUnit(event.target.value)} 
+              value={newUnit}
+              id="unit" 
+              type="text"
+            />
             <br /><br />
-            <button>Save</button>
-
+            <button 
+              onClick={(event) => addItem(event)}
+              type="submit"
+            >
+              Save
+            </button>
           </form>
         </section>
+
+        {/* Shopping list component */}
         <section>
           <h2>Shopping List</h2>
           <button>Reset</button>
           &nbsp; &nbsp;
           <button>Clear</button>
           <div>
-            <div className="notice">
-              <h3>Apples</h3>
-              <p>5 lbs.</p>
-              <button>Buy</button>
-              &nbsp;&nbsp;
-              <button>Remove</button>
-            </div>           
-            <div className="notice">
-              <h3>Apples</h3>
-              <p>5 lbs.</p>
-              <button>Buy</button>
-              &nbsp;&nbsp;
-              <button>Remove</button>
-            </div>           
-            <div className="notice">
-              <h3>Apples</h3>
-              <p>5 lbs.</p>
-              <button>Buy</button>
-              &nbsp;&nbsp;
-              <button>Remove</button>
-            </div>           
-            <div className="notice">
-              <h3>Apples</h3>
-              <p>5 lbs.</p>
-              <button>Buy</button>
-              &nbsp;&nbsp;
-              <button>Remove</button>
-            </div>           
+            {/* Item child component */}
+            {
+              Array.isArray(items) && items.map((item) => {
+                return (
+                  <div key={item.id} className="notice">
+                    <h3>{item.name}</h3>
+                    <p>{item.quantity} {item.unit}</p>
+                    <button>Buy</button>
+                    &nbsp;&nbsp;
+                    <button>Remove</button>
+                  </div>
+                );
+              })
+            }
           </div>
         </section>
+
       </main>
     </div>
   );
